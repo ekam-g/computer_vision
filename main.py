bce06739d116712ab7388913e270bcb1a2cfdff2
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-14 10:00:58 -0400

    vsdf

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 import cv2
 
-# Open the device at the ID 0
-import self as self
+stream = cv2.VideoCapture(0) # # 0 means read from local camera.
 
-cap = cv2.VideoCapture(0)
-
-# Check whether user selected camera is opened successfully.
-
-if not (cap.isOpened()):
-    print('Could not open video device')
-
-# To set the resolution
-
-self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 160)
-
-self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 160)
-
-while (True):
-    ret, frame = cap.read()
-
-    # Display the resulting frame
-
-    cv2.imshow('preview', 'frame')
-
-    # Waits for a user input to quit the application
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-
-cap.release()
-
-cv2.destroyAllWindows()
+i = 0
+while i <= 10:
+    def plot_boxes(self, results, frame):
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            # If score is less than 0.2 we avoid making a prediction.
+            if row[4] < 0.2:
+                continue
+            x1 = int(row[0]*x_shape)
+            y1 = int(row[1]*y_shape)
+            x2 = int(row[2]*x_shape)
+            y2 = int(row[3]*y_shape)
+            bgr = (0, 255, 0) # color of the box
+            classes = self.model.names # Get the name of label index
+            label_font = cv2.FONT_HERSHEY_SIMPLEX #Font for the label.
+            cv2.rectangle(frame, \
+                          (x1, y1), (x2, y2), \
+                           bgr, 2) #Plot the boxes
+            cv2.putText(frame,\
+                        classes[labels[i]], \
+                        (x1, y1), \
+                        label_font, 0.9, bgr, 2) #Put a label over box.
+            return frame
